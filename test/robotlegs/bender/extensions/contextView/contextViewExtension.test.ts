a217d0d4d23abb61bdf3857a02ba0b69e08457e3
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-09-19 14:09:32 +0200

    add unit tests base class for ContextViewExtension

---
+++ test/robotlegs/bender/extensions/contextView/contextViewExtension.test.ts
@@ -0,0 +1,37 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2016 Goodgame Studios. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import { assert } from "chai";
+
+import {
+    IContext,
+    Context,
+    LifecycleError
+} from "robotlegs";
+
+import { ContextViewExtension } from "../../../../../src/robotlegs/bender/extensions/contextView/ContextViewExtension";
+
+describe("ContextViewExtension", () => {
+
+    let context: IContext;
+
+    beforeEach(() => {
+        context = new Context();
+    });
+
+    afterEach(() => {
+        context = null;
+    });
+
+    it("installing after initialization throws error", () => {
+        function installExtensionAfterInitialization(): void {
+            context.initialize();
+            context.install(ContextViewExtension);
+        }
+        assert.throws(installExtensionAfterInitialization, LifecycleError);
+    });
+});
