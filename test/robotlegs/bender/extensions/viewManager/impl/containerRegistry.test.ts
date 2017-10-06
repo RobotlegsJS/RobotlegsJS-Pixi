6762688588fa603e07e35c7954f00de905edd6ba
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-10-06 21:43:53 +0200

    add unit tests for addContainer method

--- test/robotlegs/bender/extensions/viewManager/impl/containerRegistry.test.ts
+++ test/robotlegs/bender/extensions/viewManager/impl/containerRegistry.test.ts
@@ -82,6 +82,31 @@ describe("ContainerRegistry", () => {
         }
     });
 
+    it("binding_returns_with_correct_interested_parent_chain", () => {
+        let searchTrees: TreeContainer[] = createTrees(5, 4);
+
+        registry.addContainer(searchTrees[0]);
+        registry.addContainer(searchTrees[1]);
+        registry.addContainer(searchTrees[1].treeChildren[3]);
+
+        let searchItem: Sprite =
+            searchTrees[1].treeChildren[3].treeChildren[3].treeChildren[3]
+                .treeChildren[3];
+        let result: ContainerBinding = registry.findParentBinding(searchItem);
+
+        assert.equal(
+            searchTrees[1].treeChildren[3],
+            result.container,
+            "Binding returns with correct container view"
+        );
+        assert.equal(
+            searchTrees[1],
+            result.parent.container,
+            "Binding returns with correct container parent view"
+        );
+        assert.equal(null, result.parent.parent, "Further parents are null");
+    });
+
     function createTrees(
         treeDepth: number,
         treeWidth: number
