6b7ba23c9c1f4c6734151740878b376148b94c79
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-10-24 18:32:22 +0200

    use map methods properly

--- src/robotlegs/bender/extensions/mediatorMap/impl/MediatorViewHandler.ts
+++ src/robotlegs/bender/extensions/mediatorMap/impl/MediatorViewHandler.ts
@@ -24,7 +24,7 @@ export class MediatorViewHandler implements IViewHandler {
 
     private _knownMappings: Map<
         FunctionConstructor,
-        IMediatorMapping[]
+        IMediatorMapping[] | boolean
     > = new Map<FunctionConstructor, IMediatorMapping[]>();
 
     private _factory: MediatorFactory;
@@ -53,7 +53,7 @@ export class MediatorViewHandler implements IViewHandler {
             return;
         }
         this._mappings.push(mapping);
-        this.flushCache();
+        this._knownMappings.clear();
     }
 
     /**
@@ -65,7 +65,7 @@ export class MediatorViewHandler implements IViewHandler {
             return;
         }
         this._mappings.splice(index, 1);
-        this.flushCache();
+        this._knownMappings.clear();
     }
 
     /**
@@ -92,43 +92,36 @@ export class MediatorViewHandler implements IViewHandler {
     /* Private Functions                                                          */
     /*============================================================================*/
 
-    private flushCache(): void {
-        this._knownMappings = new Map<
-            FunctionConstructor,
-            IMediatorMapping[]
-        >();
-    }
-
     private getInterestedMappingsFor(
         item: Object,
         type: any
     ): IMediatorMapping[] {
-        let mapping: IMediatorMapping;
-
         // we've seen this type before and nobody was interested
-        if (this._knownMappings[type] === false) {
+        if (this._knownMappings.get(type) === false) {
             return null;
         }
 
         // we haven't seen this type before
-        if (this._knownMappings[type] === undefined) {
-            this._knownMappings[type] = false;
+        if (this._knownMappings.get(type) === undefined) {
+            this._knownMappings.set(type, false);
             for (let i in this._mappings) {
                 let mapping: IMediatorMapping = this._mappings[i];
                 if (mapping.matcher.matches(item)) {
-                    if (!this._knownMappings[type]) {
-                        this._knownMappings[type] = [];
+                    if (!this._knownMappings.get(type)) {
+                        this._knownMappings.set(type, []);
                     }
-                    this._knownMappings[type].push(mapping);
+                    (this._knownMappings.get(type) as IMediatorMapping[]).push(
+                        mapping
+                    );
                 }
             }
             // nobody cares, let's get out of here
-            if (this._knownMappings[type] === false) {
+            if (this._knownMappings.get(type) === false) {
                 return null;
             }
         }
 
         // these mappings really do care
-        return this._knownMappings[type];
+        return this._knownMappings.get(type) as IMediatorMapping[];
     }
 }
