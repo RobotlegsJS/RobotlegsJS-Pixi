f864f868a3f3c67871c62fb0ebe7f314de5d4460
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-10-30 21:20:25 +0100

    add unit tests for ContextViewListenerConfig class

---
+++ test/robotlegs/bender/extensions/contextView/impl/contextViewListenerConfig.test.ts
@@ -0,0 +1,54 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017 RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import "../../../../../entry";
+
+import { assert } from "chai";
+
+import { Container } from "pixi.js";
+
+import {
+    IContextView,
+    IViewManager,
+    ContextView,
+    ContextViewListenerConfig
+} from "../../../../../../src";
+
+import { ContainerRegistry } from "../../../../../../src/robotlegs/bender/extensions/viewManager/impl/ContainerRegistry";
+import { ViewManager } from "../../../../../../src/robotlegs/bender/extensions/viewManager/impl/ViewManager";
+
+describe("ContextViewListenerConfig", () => {
+    let container: Container;
+    let contextView: IContextView;
+    let containerRegistry: ContainerRegistry;
+    let viewManager: ViewManager;
+    let contextViewListenerConfig: ContextViewListenerConfig;
+
+    beforeEach(() => {
+        container = new Container();
+        contextView = new ContextView(container);
+        containerRegistry = new ContainerRegistry();
+        viewManager = new ViewManager(containerRegistry);
+        contextViewListenerConfig = new ContextViewListenerConfig(
+            contextView,
+            viewManager
+        );
+    });
+
+    afterEach(() => {
+        contextView = null;
+        container = null;
+        containerRegistry = null;
+        viewManager = null;
+        contextViewListenerConfig = null;
+    });
+
+    it("container_is_stored", () => {
+        contextViewListenerConfig.configure();
+        assert.deepEqual(viewManager.containers, [container]);
+    });
+});
