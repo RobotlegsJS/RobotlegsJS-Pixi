4c19f0ed987c8bbb662c01c16d07e6a036e028cb
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-08-29 20:53:37 +0200

    initialize observer factory

--- test/robotlegs/bender/extensions/mediatorMap/impl/mediatorViewHandler.test.ts
+++ test/robotlegs/bender/extensions/mediatorMap/impl/mediatorViewHandler.test.ts
@@ -11,7 +11,13 @@ import { assert } from "chai";
 
 import { Container, Sprite } from "pixi.js";
 
-import { IContext, IInjector, ITypeFilter, Context, TypeMatcher } from "@robotlegsjs/core";
+import { interfaces, IContext, IInjector, ITypeFilter, Context, TypeMatcher } from "@robotlegsjs/core";
+
+import { DisplayObjectObserver } from "../../../../../../src/robotlegs/bender/bundles/pixi/observer/DisplayObjectObserver";
+
+import { IDisplayObject } from "../../../../../../src/robotlegs/bender/displayList/api/IDisplayObject";
+import { IDisplayObjectObserver } from "../../../../../../src/robotlegs/bender/displayList/api/IDisplayObjectObserver";
+import { IDisplayObjectObserverFactory } from "../../../../../../src/robotlegs/bender/displayList/api/IDisplayObjectObserverFactory";
 
 import { MediatorFactory } from "../../../../../../src/robotlegs/bender/extensions/mediatorMap/impl/MediatorFactory";
 import { MediatorMapping } from "../../../../../../src/robotlegs/bender/extensions/mediatorMap/impl/MediatorMapping";
@@ -29,6 +35,11 @@ describe("MediatorViewHandler", () => {
     beforeEach(() => {
         context = new Context();
         injector = context.injector;
+        injector.bind<interfaces.Factory<IDisplayObjectObserver>>(IDisplayObjectObserverFactory).toFactory<IDisplayObjectObserver>(() => {
+            return (view: IDisplayObject, useCapture: boolean): IDisplayObjectObserver => {
+                return new DisplayObjectObserver(view, useCapture);
+            };
+        });
         factory = new MediatorFactory(injector);
         handler = new MediatorViewHandler(factory);
         context.initialize();
