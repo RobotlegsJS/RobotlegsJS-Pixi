dd12f9de4ef5f843fca29a0622cc1935c8ac9b59
Tiago Schenkel <tiago.schenkel@gmail.com>
2019-08-17 17:24:08 +0200

    remove open-browser-webpack-plugin dependency

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -1,52 +1,46 @@
 const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const OpenBrowserPlugin = require("open-browser-webpack-plugin");
 const SimpleProgressPlugin = require("webpack-simple-progress-plugin");
 
 module.exports = options => {
-  return {
-    mode: "development",
+    return {
+        mode: "development",
 
-    entry: {
-      main: path.resolve("example/index.ts")
-    },
+        entry: {
+            main: path.resolve("example/index.ts")
+        },
 
-    output: {
-      path: __dirname + "/dist",
-      filename: "game.[hash].js"
-    },
+        output: {
+            path: __dirname + "/dist",
+            filename: "game.[hash].js"
+        },
 
-    devtool: "source-map",
+        devtool: "source-map",
 
-    module: {
-      rules: [
-        { test: /\.ts$/, loader: "ts-loader" }
-      ]
-    },
+        module: {
+            rules: [{ test: /\.ts$/, loader: "ts-loader" }]
+        },
 
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve("static/index.html"),
-        inject: false
-      }),
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve("static/index.html"),
+                inject: false
+            }),
 
-      new SimpleProgressPlugin(),
+            new SimpleProgressPlugin()
+        ],
 
-      new OpenBrowserPlugin({ url: "http://0.0.0.0:8080/webpack-dev-server/" })
-    ],
+        resolve: {
+            extensions: [".ts", ".js", ".json"]
+        },
 
-    resolve: {
-      extensions: [".ts", ".js", ".json"]
-    },
-
-    devServer: {
-      host: "0.0.0.0",
-      contentBase: path.join(__dirname, "static"),
-      hot: true,
-      disableHostCheck: true,
-      inline:false
-    }
-
-  }
+        devServer: {
+            host: "0.0.0.0",
+            contentBase: path.join(__dirname, "static"),
+            hot: true,
+            disableHostCheck: true,
+            inline: false
+        }
+    };
 };
